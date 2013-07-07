4cb91bbadb6c578cbb14ad8ce658a0f0fca57b2f
KimSangYoon <tkdvmf423@gmail.com>
2013-07-07 16:07:01 +0900

    Initial Commit

---
+++ OBT/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  OBTAppController.h
+//  OBT
+//
+//  Created by KimSangYoon on 13. 7. 7..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ OBT/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  OBTAppController.h
+//  OBT
+//
+//  Created by KimSangYoon on 13. 7. 7..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
